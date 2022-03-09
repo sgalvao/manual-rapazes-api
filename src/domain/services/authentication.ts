767589acb702ec5a4505f5bfbb3d44da92164ae0
Silvao <silvio.gabrielgt2000@hotmail.com>
2022-03-08 22:56:38 -0300

    Errors message added

--- src/domain/services/authentication.ts
+++ src/domain/services/authentication.ts
@@ -11,26 +11,18 @@ export class AuthenticationService {
   ) {}
 
   async auth(params: AuthenticationService.Params) {
-    console.log(
-      "🚀 ~ file: authentication.ts ~ line 14 ~ AuthenticationService ~ auth ~ params",
-      params
-    );
     const user = await this.usersRepository.findByEmail(params.email);
     if (!user) {
-      throw new Error("User not found");
+      throw new Error("Usuário ou senha invalida!");
     }
 
     const isValidPassword = await this.hashProvider.compareHash(
       params.password,
       user.password
     );
-    console.log(
-      "🚀 ~ file: authentication.ts ~ line 27 ~ AuthenticationService ~ auth ~ isValidPassword",
-      isValidPassword
-    );
 
     if (!isValidPassword) {
-      throw new Error(" password or username invalid!");
+      throw new Error("Usuário ou senha invalida!");
     }
 
     const token = this.jwtProvider.encryptToken(user.email, env.jwtSecret);
